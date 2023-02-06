ca8f8ef455812317bcd4e70d7de483c397d70229
hamzaTCF <hamza.test.cf@gmail.com>
2023-02-06 13:49:18 +0200

    added mock-data + refactored CitySearch tests

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -4,15 +4,17 @@ import { render } from '@testing-library/react';
 import CitySearch from '../components/CitySearch';
 
 describe('<CitySearch /> component', () => {
+  let CitySearchComponent;
+  beforeEach(() => {
+    CitySearchComponent = render(<CitySearch />);
+  });
   test('renders text input', () => {
-    const CitySearchComponent = render(<CitySearch />);
     const cityTextBox = CitySearchComponent.getByRole('textbox');
     expect(cityTextBox).toBeInTheDocument();
     expect(cityTextBox).toHaveClass('city');
   });
 
   test('renders a list of suggestions', () => {
-    const CitySearchComponent = render(<CitySearch />);
     const suggestionList = CitySearchComponent.getByRole('list')
     expect(suggestionList).toBeInTheDocument();
     expect(suggestionList).toHaveClass('suggestions');
